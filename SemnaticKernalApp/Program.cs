97eef2f0e71dc48566e8ea2e7a11adf0ec6cfee3
LAFITAU Benjamin <Benjamin.lafitau@users.noreply.github.com>
2024-10-01 09:37:01 +0200

    init

---
+++ SemnaticKernalApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("OPENIA_API_KEY");
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            throw new InvalidOperationException("OPENIA_API_KEY environment variable is not set or empty.");
+        }
+
+        var kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion("gpt-3.5-turbo", apiKey)
+            .Build();
+
+        Console.ReadKey();
+    }
+}
